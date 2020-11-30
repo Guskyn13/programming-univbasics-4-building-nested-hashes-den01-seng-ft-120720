66048fe0fa88c61255f0b0766e5ea6248e797fdc
Nathan K Gusky <n.gusky88@gmail.com>
2020-11-30 19:18:35 +0000

    Automatically backed up by Learn

--- final_hash.rb
+++ final_hash.rb
@@ -1,10 +1,10 @@
 def base_hash
-:railroads = {}
+"railroads" = {}
 end
 def monopoly_with_second_tier
   base_hash => {pieces: "4", rent_in_dollars: {}, names: {}}
 end
-def monopoly_with_third_tier(hash, key)
+def monopoly_with_third_tier
 end
 def monopoly_with_fourth_tier(hash, key)
 end
