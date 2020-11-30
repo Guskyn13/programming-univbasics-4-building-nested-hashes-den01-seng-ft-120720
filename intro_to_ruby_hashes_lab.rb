a2ab9ec1d293865029ee3fa6f763e1b1e903f1a1
Nathan K Gusky <n.gusky88@gmail.com>
2020-11-30 19:55:06 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,8 +1,6 @@
 def base_hash
   monopoly = {
-    :railroads => {
-
-    }
+    :railroads => {}
   }
 
 end
@@ -12,9 +10,7 @@ def monopoly_with_second_tier
     :railroads => {
       :pieces => 4
       :rent_in_dollars => {}
-      :names => {
-
-      }
+      :names => {}
     }
   }
 end
