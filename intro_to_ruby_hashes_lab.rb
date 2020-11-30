4f988a621f421efaca19ef0deb94fc949c5b226c
Nathan K Gusky <n.gusky88@gmail.com>
2020-11-30 19:47:56 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -4,20 +4,18 @@ def base_hash
 
     }
   }
+
 end
 
 def monopoly_with_second_tier
   monopoly = {
     :railroads => {
-      pieces: => "4",
+      :pieces => 4
       :rent_in_dollars => {
-        one_piece_owned: => "25",
-        two_pieces_owned: => "50",
-        three_pieces_owned: => "100",
-        four_pieces_owned: => "200"
+        
       }
       :names => {
-
+        
       }
     }
   }
@@ -26,15 +24,20 @@ end
 def monopoly_with_third_tier
   monopoly = {
     :railroads => {
-      :pieces => 4
+      pieces: => "4",
       :rent_in_dollars => {
-
+        one_piece_owned: => "25",
+        two_pieces_owned: => "50",
+        three_pieces_owned: => "100",
+        four_pieces_owned: => "200"  
       }
       :names => {
-
+        
       }
     }
   }
+
+
 end
 
 def monopoly_with_fourth_tier
